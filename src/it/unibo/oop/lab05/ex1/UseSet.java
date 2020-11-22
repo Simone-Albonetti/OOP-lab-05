eead3408f1526438f7dc313386f823641a0ad871
Simone Albonetti <simone.albonetti@studio.unibo.it>
2020-11-22 17:57:42 +0100

    Done ex1

--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab05.ex1;
 
+import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link Set}.
@@ -21,18 +23,47 @@ public final class UseSet {
          * order:
          * 
          * 1) Builds a TreeSet containing Strings
-         * 
+         */
+    	Set<String> tSet = new TreeSet<>();
+    	/*
          * 2) Populates such Collection with all the Strings ranging from "1" to
          * "20"
-         * 
+         */
+    	for (int i = 1 ; i<=20 ; i++) {
+    		tSet.add("" + i);
+    	}
+    	/*
          * 3) Prints its content
-         * 
+         */
+    	System.out.println(tSet);
+    	
+    	/*
          * 4) Removes all those strings whose represented number is divisible by
          * three
-         * 
-         * 5) Prints the content of the Set using a for-each costruct
-         * 
-         * 6) Verifies if all the numbers left in the set are even
          */
+   
+    	Iterator<String> it = tSet.iterator();
+    	while(it.hasNext()) {
+    		if( Integer.parseInt(it.next()) % 3 == 0){
+    			it.remove();
+    		}
+    	}
+    	/*
+         * 5) Prints the content of the Set using a for-each costruct
+         */
+    	for(String s : tSet) {
+    		System.out.print(s + ", ");
+    	}
+    	
+    	/*
+         * 6) Verifies if all the numbers left in the set are even
+    	*/
+    	var newset = new TreeSet<>();
+    	for (int i = 2 ; i<= 20 ; i += 2) {
+    		newset.add(""+i);
+    	}
+    	
+    	System.out.println("\nEven:" + newset);
+    	System.out.println(tSet.containsAll(newset));
     }
-}
+ }
